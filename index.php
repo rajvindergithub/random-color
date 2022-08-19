6c052a227735f3aa4d197454ab6090aafc905b4b
rajvindergithub <rajvindergithub@gmail.com>
2022-08-19 22:41:28 +0530

    ajax data send to submit.php and show result on index

--- index.php
+++ index.php
@@ -9,6 +9,11 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" href="">
 	<link rel="icon" type="image/x-icon" href=""/>
+    
+    <style type="text/css">
+        .colorDivResult{ height: 75px; width: 75px; border-radius: 50%; background-color: #efefef;  }
+    </style>
+    
 </head>
 
 <body>
@@ -16,10 +21,58 @@
     <div><input type="text" placeholder="Enter" id="colorInputField"></div>
     
     <div>
-        <button id="GenerateColorShades">Generate Now</button>
+        <button id="GenerateColorShades" onclick="generateColorShades();">Generate Now</button>
     </div>
+    
+    <section id="colorResult">
+        <div id="hueColorResult">
+            <div class="colorResultDivMain">
+                <div class="colorDivResult"></div>
+                <div class="colorDivTextarea">
+                    <textarea></textarea>
+                </div>
+            </div>
+        </div>
+        <div id="luminosityColorResult"></div>
+    </section>
     
     
 </body>
+    
+    <script type="text/javascript" src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 
+    <script type="text/javascript">
+    
+        function generateColorShades(){
+            
+            var colorName = jQuery('#colorInputField').val();
+            
+            var data = {colorName: colorName};
+            
+            $.ajax({
+                url : 'submit.php',
+                type: 'POST',
+                data : data,
+                success: function(response){
+                    
+                    var hueColors = response.hue;
+                    var luminosityColors = response.luminosity;
+                    
+                    console.log(hueColors);
+                    
+                    var hue = '';
+                    
+                    for(var i = 0; i < hueColors.length; i++){
+                        hue += `<div class="colorDivResult" style="background-color: ${hueColors[i]}">${hueColors[i]}</div>`;
+                    }
+                    
+                    jQuery('#hueColorResult').html(hue);
+                    
+                }
+            });
+            
+        }
+    
+    </script>
+    
 </html>
---
+++ submit.php
@@ -0,0 +1,38 @@
+<?php 
+
+use \Colors\RandomColor;
+
+include 'vendor/autoload.php';
+
+// Returns a hex code for an attractive color
+RandomColor::one(); 
+
+if(isset($_POST['colorName'])){
+    
+    $color = $_POST['colorName']; 
+    
+        // Returns an array of ten green colors
+        $randomColor = RandomColor::many(10, array(
+           'hue' => $color
+        ));
+
+        $randomColorLum = RandomColor::many(10, array(
+           'luminosity' => $color
+        ));
+
+        //echo '<div>Hello</div>'; 
+
+        //print_r($randomColor);
+        //echo '<br />';
+
+        //print_r($randomColorLum);
+
+        $arrayColor = array("hue"=>$randomColor, "luminosity"=> $randomColorLum);
+
+        header('Content-Type: application/json; charset=utf-8');
+
+        echo json_encode($arrayColor);
+
+}
+
+?>
